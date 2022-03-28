1fdaf0face7ab06b33fe7287e4d41a28f9ebef78
IIIIII <github@bigly.dog>
2022-03-27 23:30:19 -0700

    ok

--- agp/da.py
+++ agp/da.py
@@ -1,27 +1,21 @@
 from asyncio import create_subprocess_exec, to_thread
 from asyncio.subprocess import DEVNULL, PIPE
-from dataclasses import dataclass
 from http.client import HTTPResponse
 from os import environ
 from pathlib import Path, PurePath
-from typing import Any, Dict, Optional, Union, cast
+from subprocess import CompletedProcess
+from typing import Any, Mapping, Tuple, Union, cast
 from urllib.request import Request, build_opener
 
-
-@dataclass(frozen=True)
-class ProcReturn:
-    code: int
-    out: str
-    err: str
+_OPENER = build_opener()
 
 
 async def call(
     prog: str,
     *args: str,
-    env: Dict[str, str] = {},
+    env: Mapping[str, str] = {},
     cwd: PurePath = Path.cwd(),
-    expect: Optional[int] = None
-) -> ProcReturn:
+) -> CompletedProcess[str]:
     envi = {**environ, **env}
     proc = await create_subprocess_exec(
         prog, *args, stdin=DEVNULL, stdout=PIPE, stderr=PIPE, env=envi, cwd=cwd
@@ -30,18 +24,21 @@ async def call(
     code = cast(int, proc.returncode)
     out, err = stdout.decode(), stderr.decode()
 
-    if expect is not None and code != expect:
-        raise RuntimeError(err)
-    else:
-        return ProcReturn(code=code, out=out, err=err)
+    p = CompletedProcess(
+        args=(prog, *args),
+        returncode=code,
+        stdout=out,
+        stderr=err,
+    )
+    p.check_returncode()
+    return p
 
 
-async def req(req: Union[Request, str]) -> Any:
-    def _req() -> Any:
-        opener = build_opener()
-        with opener.open(req) as resp:
+async def req(req: Union[Request, str]) -> Tuple[HTTPResponse, Any]:
+    def _req() -> Tuple[HTTPResponse, Any]:
+        with _OPENER.open(req) as resp:
             resp = cast(HTTPResponse, resp)
-            return resp.read()
+            return resp, resp.read()
 
     msg = await to_thread(_req)
     return msg
--- agp/github.py
+++ agp/github.py
@@ -4,38 +4,73 @@ from json import loads
 from pathlib import Path, PurePosixPath
 from posixpath import normcase
 from string import Template
-from typing import AsyncIterator, Optional, Sequence
+from typing import AsyncIterator, MutableSet, Optional, Sequence
 from urllib.error import HTTPError
 
 from .da import call, req
 
 
 @dataclass(frozen=True)
-class RepoInfo:
+class _RepoInfo:
     name: str
     full_name: str
     default_branch: str
     private: bool
+    archived: bool
 
 
-async def ls_repos(username: str) -> Sequence[RepoInfo]:
-    # TODO -- Use Header <link> for pagination API
-    uri = f"https://api.github.com/users/{username}/repos?per_page=100"
-    data = await req(uri)
-    repos = loads(data)
-    names = tuple(
-        RepoInfo(
+def _page(link: str) -> Optional[str]:
+    for sections in link.split(","):
+        uri, *params = sections.split(";")
+        for param in params:
+            key, _, value = param.strip().partition("=")
+            val = (
+                value.removeprefix('"').removesuffix('"')
+                if value.startswith('"') and value.endswith('"')
+                else value
+            )
+            if key == "rel" and val == "next":
+                return uri.removeprefix("<").removesuffix(">")
+    else:
+        return None
+
+
+async def _repos(uri: str) -> AsyncIterator[_RepoInfo]:
+    pages: MutableSet[str] = set()
+    resp, raw = await req(uri)
+    for key, val in resp.getheaders():
+        if key.casefold() == "link":
+            page = _page(val)
+            if page:
+                pages.add(page)
+
+    json = loads(raw)
+
+    repos = tuple(
+        _RepoInfo(
             name=repo["name"],
             full_name=repo["full_name"],
             default_branch=repo["default_branch"],
             private=repo["private"],
+            archived=repo["archived"],
         )
-        for repo in repos
+        for repo in json
     )
-    return names
+
+    for repo in repos:
+        yield repo
+    for page in pages:
+        async for repo in _repos(page):
+            yield repo
 
 
-async def check_exists(repo: RepoInfo, resource: str) -> bool:
+def ls_repos(username: str) -> AsyncIterator[_RepoInfo]:
+    # TODO -- Use Header <link> for pagination API
+    uri = f"https://api.github.com/users/{username}/repos?per_page=100"
+    return _repos(uri)
+
+
+async def check_exists(repo: _RepoInfo, resource: str) -> bool:
     uri = f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/{resource}"
     try:
         await req(uri)
@@ -45,22 +80,25 @@ async def check_exists(repo: RepoInfo, resource: str) -> bool:
         return True
 
 
-async def elligible_repos(username: str) -> Sequence[RepoInfo]:
+async def elligible_repos(username: str) -> Sequence[_RepoInfo]:
     no_agp = normcase(PurePosixPath(".github") / ".noagp")
-    repos = await ls_repos(username=username)
 
-    async def cont() -> AsyncIterator[RepoInfo]:
-        for repo in repos:
-            exists = await check_exists(repo, resource=no_agp)
-            if not exists:
-                yield repo
+    async def cont() -> AsyncIterator[_RepoInfo]:
+        async for repo in ls_repos(username=username):
+            if not repo.archived:
+                exists = await check_exists(repo, resource=no_agp)
+                if not exists:
+                    yield repo
 
     return [repo async for repo in cont()]
 
 
 def tokenify_repo(repo_name: str, username: str, token: Optional[str]) -> str:
-    tok = f":{token}" if token else ""
-    tokenized = f"https://{username}{tok}@github.com/{repo_name}.git"
+    if token:
+        tokenized = f"https://{username}:{token}@github.com/{repo_name}.git"
+    else:
+        tokenized = f"git@github.com:{repo_name}.git"
+
     return tokenized
 
 
@@ -74,13 +112,13 @@ $time
 
 
 async def increment_push(
-    repo: RepoInfo,
+    repo: _RepoInfo,
     username: str,
     token: Optional[str],
     base_dir: Path,
     bot_name: str,
     bot_email: str,
-) -> RepoInfo:
+) -> _RepoInfo:
     git_uri = tokenify_repo(repo.full_name, username=username, token=token)
     repo_dir = base_dir / repo.name
     spec_dir = repo_dir / ".github"
@@ -90,15 +128,15 @@ async def increment_push(
     long_msg = Template(TEMPLATE).substitute(time=time)
     msg = f"CI (AGP) - {time}"
 
-    await call("git", "clone", "--depth=1", git_uri, cwd=base_dir, expect=0)
+    await call("git", "clone", "--depth=1", git_uri, cwd=base_dir)
 
     spec_dir.mkdir(parents=True, exist_ok=True)
     inc_file.write_text(long_msg)
 
-    await call("git", "config", "user.email", bot_email, cwd=repo_dir, expect=0)
-    await call("git", "config", "user.name", bot_name, cwd=repo_dir, expect=0)
-    await call("git", "add", "-A", cwd=repo_dir, expect=0)
-    await call("git", "commit", "-m", msg, cwd=repo_dir, expect=0)
-    await call("git", "push", "--force", cwd=repo_dir, expect=0)
+    await call("git", "config", "user.email", bot_email, cwd=repo_dir)
+    await call("git", "config", "user.name", bot_name, cwd=repo_dir)
+    await call("git", "add", "-A", cwd=repo_dir)
+    await call("git", "commit", "-m", msg, cwd=repo_dir)
+    await call("git", "push", "--force", cwd=repo_dir)
 
     return repo
